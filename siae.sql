962aaef5cdb33bba9b26b6b981057732604a6cd5
esdeath3x <66812479+esdeath3x@users.noreply.github.com>
2020-08-04 19:51:12 -0500

    avance perfiles

--- siae.sql
+++ siae.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-07-2020 a las 06:18:02
+-- Tiempo de generación: 05-08-2020 a las 01:42:08
 -- Versión del servidor: 10.4.6-MariaDB
 -- Versión de PHP: 7.3.9
 
@@ -45,6 +45,58 @@ INSERT INTO `roles` (`id`, `rol`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `slider`
+--
+
+CREATE TABLE `slider` (
+  `id` int(11) NOT NULL,
+  `img_slider` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `posicion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `slider`
+--
+
+INSERT INTO `slider` (`id`, `img_slider`, `posicion`) VALUES
+(1, '1.jpg', 1),
+(2, '2.jpg', 2),
+(3, '3.jpg', 3),
+(4, '4.jpg', 4),
+(5, '5.jpg', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `talleres`
+--
+
+CREATE TABLE `talleres` (
+  `id` int(11) NOT NULL,
+  `taller` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `nombreRepresentativo` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `descripcion` text COLLATE utf8_spanish_ci NOT NULL,
+  `horario` text COLLATE utf8_spanish_ci NOT NULL,
+  `mtro_asignado` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `categoria` enum('cultural','deportivo','civico') COLLATE utf8_spanish_ci NOT NULL,
+  `img1` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `img2` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `img3` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `img4` varchar(20) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `talleres`
+--
+
+INSERT INTO `talleres` (`id`, `taller`, `nombreRepresentativo`, `descripcion`, `horario`, `mtro_asignado`, `categoria`, `img1`, `img2`, `img3`, `img4`) VALUES
+(1, 'Porristas', 'Guerreros', '¿Te gustaría ser la siguiente Britney Allen? \r\nNo lo pienses más y únete a nuestro taller de animación. ', 'Lunes: 5pm-8pm\r\nJueves: 7am-10am\r\nSabado: 2pm-4pm', '', 'cultural', '1.jpg', '', '', ''),
+(2, 'Ballet folklorico', 'Ballet', 'Aqui entras a zapatear', 'Jueves de 4am-6am', '', 'cultural', '2.jpg', '', '', ''),
+(3, 'Batucada', 'Los halcones', 'Dolores de cabeza gratis', 'Jueves de 4am-6am', '', 'cultural', '3.jpg', '', '', '');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuario`
 --
 
@@ -69,6 +121,18 @@ ALTER TABLE `roles`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `slider`
+--
+ALTER TABLE `slider`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `talleres`
+--
+ALTER TABLE `talleres`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -76,6 +140,18 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT de la tabla `roles`
 --
 ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `slider`
+--
+ALTER TABLE `slider`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `talleres`
+--
+ALTER TABLE `talleres`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
