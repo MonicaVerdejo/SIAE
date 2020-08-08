9812b8b9443f02cf67c94203cd52de1f51c5c859
esdeath3x <66812479+esdeath3x@users.noreply.github.com>
2020-08-08 04:00:01 -0500

    avance funciones alumnos

--- alumno.php
+++ alumno.php
@@ -10,6 +10,19 @@ if (!isset($_SESSION['rol'])) {
         header('location: perfiles.html');
     }
 }
+
+
+$query = $db->connect()->prepare("SELECT talleres.taller, talleres.direccion, talleres.mtro_asignado, maestro.nombre FROM talleres JOIN maestro join alumnos ON talleres.id = alumnos.taller_id and maestro.taller_asignado = talleres.mtro_asignado;");
+$query->execute();
+
+$opcion = $query->fetch(PDO::FETCH_NUM);
+
+//session_destroy('imagen_profile');
+//session_start();
+$_SESSION['taller'] = $opcion[0];
+$direccion = $opcion[1];
+$mtroasignado = $opcion[3];
+
 ?>
 
 <!DOCTYPE html>
@@ -30,6 +43,7 @@ if (!isset($_SESSION['rol'])) {
     <link rel="stylesheet" href="TABLA/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css" type="text/css">
     <link rel="stylesheet" href="TABLA/plugins/datatables-responsive/css/responsive.bootstrap4.min.css" type="text/css">
     <link rel="stylesheet" href="css/styles2.css">
+
 </head>
 
 <body>
@@ -52,29 +66,23 @@ if (!isset($_SESSION['rol'])) {
                             <li class="header-menu">
                                 <span>General</span>
                             </li>
-                            <li>
+                            <li id="alumno_inicio">
                                 <a href="alumno.php">
                                     <i class="fa fa-tachometer-alt"></i>
                                     <span>Inicio</span>
                                 </a>
                             </li>
-                            <li>
-                                <a href="alumno_horario.php">
+                            <li id="alumno_horario">
+                                <a href="#horario">
                                     <i class="fas fa-table"></i>
                                     <span>Horario</span>
                                 </a>
 
                             </li>
-                            <li>
-                                <a href="alumno_mensajes.php">
+                            <li id="alumno_mensajes">
+                                <a href="#alumno_mensajes">
                                     <i class="fa fa-comment" aria-hidden="true"></i>
                                     <span>Mensajes</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="datos_alumno.php">
-                                    <i class="fas fa-graduation-cap"></i>
-                                    <span>Datos generales</span>
                                 </a>
                             </li>
                             <li>
@@ -99,14 +107,155 @@ if (!isset($_SESSION['rol'])) {
                 <!--Inicio-->
                 <div class="section section-lg ">
                     <h1>
-                        Bienvenido <?php echo ($_SESSION['usuario']); ?>
+                        <?php
+                        if ($_SESSION['sexo'] == 'F') {
+                        ?> Bienvenida <?php echo ($_SESSION['nombre']);
+                                    } else {
+                                        ?> Bienvenido <?php echo ($_SESSION['nombre']);
+                                                    }
+
+                                                        ?>
                     </h1>
+                </div>
+
+                <div class="row" id="inicio">
+                    <div class="col-lg-6 col-sm-6 col-xs-12">
+                        ESTADO DEL CURSO: <?php echo ($_SESSION['estatus']); ?>
+                    </div>
+                    <div class="col-lg-6 col-sm-6 col-xs-12">
+                        <div class="datosTaller card-1">
+                         
+                                <li>
+                                    <i class="fa fa-bookmark" aria-hidden="true"></i>
+                                    Inscrito en: <?php echo $_SESSION['taller']; ?>
+                                </li>
+                                <li>
+                                    <i class="fa fa-user-circle" aria-hidden="true"></i>
+                                    Matricula: <?php echo $_SESSION['matricula']; ?>
+                                </li>
+                                <li>
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    Carrera: <?php echo $_SESSION['carrera']; ?>
+                                    <hr>
+                                </li>
+                                <li>
+                                <i class="fa fa-id-card" aria-hidden="true"></i>
+                                    Maestro asignado: <?php echo $mtroasignado; ?>
+                                </li>
+                                <li>
+                                    <i class="fa fa-paper-plane" aria-hidden="true"></i>
+                                    Ubicación del taller: <?php echo $direccion; ?>
+                                    <hr>
+                                </li>
+                            
+                            <span>En caso de dudas consultar en el área de actividades extraescolares. Departamento de vinculación.</span>
+                        </div>
+                    </div>
                 </div>
 
                 <!--Horario-->
 
+                <div class="container" id="horario" style="display: none;">
+
+                    <section id="tabla_resultado" class="content">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-12 ">
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <h3 class="card-title text-center">Horario</h3>
+                                        </div>
+                                        <!-- /.card-header -->
+                                        <div class="card-body">
+                                            <table id="datos" class="text-center table table-bordered table-hover table-responsive">
+
+
+
+
+                                            </table>
+                                        </div>
+                                        <!-- /.card-body -->
+                                    </div>
+                                    <!-- /.card -->
+                                </div>
+                                <!-- /.col -->
+                            </div>
+                            <!-- /.row -->
+                        </div>
+                        <!-- /.container-fluid -->
+                    </section>
+
+                </div>
+
+
+
                 <!--Mensajes-->
-                <!--Datos generales-->
+
+                <section id="mensajes" style="display: none;">
+                    <div class="container-fluid">
+                        <div class="col-12 card">
+                            <div class="card-header">
+                                <h3 class="card-title">
+                                <i class="fa fa-envelope" aria-hidden="true"></i>    
+                                Mensajes</h3>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="col-lg-12 col-xs-12">
+                                <div>
+                                    <!-- /.card-header -->
+                                    <div class="card-body">
+                                        <table class="table table-bordered table-hover">
+
+                                            <tbody>
+
+                                                <?php
+                                                $taller_id = $_SESSION['taller_id'];
+
+                                                $sentencia = $db->connect()->prepare('select fecha, maestro.nombre, mensaje from maestro join mensajemaestro join talleres join alumnos
+                                                                                            on mensajemaestro.taller_id= talleres.id and alumnos.taller_id = talleres.id and talleres.id = maestro.taller_asignado 
+                                                                                                where talleres.id=:taller_id');
+                                                $sentencia->execute(['taller_id' => $taller_id]);
+                                                foreach ($sentencia as $row) {
+
+                                                ?>
+                                                    <tr>
+                                                        <td>
+                                                            <img class="" src="img/mensajes.png" width="50" height="50" alt="Mensajes">
+                                                            <br>
+                                                            <?php echo $row[0]; ?>
+                                                            <br>
+                                                            <?php echo $row[1]; ?>
+                                                        </td>
+
+                                                        <td><?php echo $row[2]; ?></td>
+
+                                                    </tr>
+                                                <?php
+
+                                                }
+
+                                                ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.row -->
+                    </div>
+                    <!-- /.container-fluid -->
+
+                </section>
+
+
+
+
+
+
+
+
+
 
 
 
@@ -125,6 +274,28 @@ if (!isset($_SESSION['rol'])) {
     <script src="js/js.js" type="text/javascript"></script>
     <script src="https://static.codepen.io/assets/common/stopExecutionOnTimeout-30d18ea41045577cdb11c797602d08e0b9c2fa407c8b81058b1c422053ad8041.js" type="text/javascript"></script>
     <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
+    <script>
+        $(document).ready(function() {
+            $("#alumno_horario").on('click', function() {
+                $("#horario").show();
+                $("#inicio").hide();
+                $("#mensajes").hide();
+                return false;
+            });
+            $("#alumno_mensajes").on('click', function() {
+                $("#horario").hide();
+                $("#inicio").hide();
+                $("#mensajes").show();
+                return false;
+            });
+            $("#alumno_inicio").on('click', function() {
+                $("#horario").hide();
+                $("#inicio").show();
+                $("#mensajes").hide();
+                return false;
+            });
+        });
+    </script>
 </body>
 
 </html>
--- login_alumno.php
+++ login_alumno.php
@@ -7,20 +7,22 @@ if (!empty($_POST['usuario']) && !empty($_POST['password'])) {
     $usuario = $_POST['usuario'];
     $password = $_POST['password'];
 
-    $query = $db->connect()->prepare('SELECT * FROM usuario WHERE usuario = :usuario');
+    $query = $db->connect()->prepare('SELECT * FROM alumnos WHERE matricula = :usuario');
     $query->execute(['usuario' => $usuario]);
 
     $row = $query->fetch(PDO::FETCH_NUM); //transforma a un arreglo que puedo usar
 
     if (($row == true) && password_verify($_POST['password'], $row[3])) { //si existe contenido valida el rol
-        $rol = $row[5];
+        $rol = $row[11];
         $_SESSION['rol'] = $rol;
-        $_SESSION['usuario'] = $row[1];
-        $_SESSION['imagen_profile'] = $row[6];
+        $_SESSION['nombre'] = $row[1];
         $_SESSION['id_user'] = $row[0];
-        $_SESSION['usuario'] = $usuario;
-
-
+        $_SESSION['estatus'] = $row[7];
+        $_SESSION['sexo'] = $row[10];
+        $_SESSION['matricula'] = $row[2];
+        $_SESSION['carrera'] = $row[6];
+        $_SESSION['semestre'] = $row[8];
+        $_SESSION['taller_id'] = $row[4];
         switch ($rol) {
             case 2:
                 header('location: alumno.php');
