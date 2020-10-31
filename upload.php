0fd2d09a097e92286e803ffcea11a306af0424e3
esdeath3x <66812479+esdeath3x@users.noreply.github.com>
2020-10-31 15:11:21 -0600

    dimensiones img_profile

--- upload.php
+++ upload.php
@@ -16,33 +16,65 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_FILES)) {
          window.location.href="administrativo.php"; </script>';
     } else {
 
+        $image = getimagesize($_FILES['file']['tmp_name']);
+        //Definir un tamaño en pixeles para la imagen...
+        $minimum = array(
+            'width' => '200',
+            'height' => '200'
+        );
 
-        $check = @getimagesize($_FILES['file']['tmp_name']);
-        if ($check !== false) {
-            $carpeta_destino = 'img/img_profile/';
-            $archivo = $carpeta_destino . $_FILES['file']['name'];
-            move_uploaded_file($_FILES['file']['tmp_name'], $archivo);
+        $maximum = array(
+            'width' => '1200',
+            'height' => '1200'
+        );
+
+        $image_width = $image[0];
+        $image_height = $image[1];
+
+       
+        if ( $image_width < $minimum['width'] || $image_height < $minimum['height'] ) {
+            //$file['error'] = $too_small;
+            //return $file;
+            echo '<script type="text/javascript">
+            alert("La imagen que seleccionaste es muy pequeña, escoge una que sea mayor de 300x300 pixeles y menor de 700x700");
+            window.location.href="administrativo.php"; </script>';
+        }elseif ( $image_width > $maximum['width'] || $image_height > $maximum['height'] ) {
+            //$file['error'] = $too_large;
+            //return $file;
+            echo '<script type="text/javascript">
+            alert("La imagen que seleccionaste es demasiado grande, escoge una que sea mayor de 300x300 pixeles y menor de 700x700");
+            window.location.href="administrativo.php"; </script>';
+        }else {
+            //return $file;
+            $check = @getimagesize($_FILES['file']['tmp_name']);
+            if ($check !== false) {
+                $carpeta_destino = 'img/img_profile/';
+                $archivo = $carpeta_destino . $_FILES['file']['name'];
+                move_uploaded_file($_FILES['file']['tmp_name'], $archivo);
+            }
+
+
+            $id = $_SESSION['id_admin'];
+
+            $sql = "UPDATE administrador SET img_profile=:img_profile WHERE id=$id";
+            $statement = $db->connect()->prepare($sql);
+            $statement->execute(array(':img_profile' => $_FILES['file']['name']));
+            //$_SESSION['imagen_profile']=$VARIABLE_HOST;
+
+            $_SESSION['img_profile'] = $statement;
+
+            $query = $db->connect()->prepare('SELECT * FROM administrador WHERE correo = :correo');
+            $query->execute(['correo' => $_SESSION['correo']]);
+
+            $row = $query->fetch(PDO::FETCH_NUM);
+
+            //session_destroy('imagen_profile');
+            //session_start();
+            $_SESSION['img_profile'] = $row[5];
+            echo "<script>location.href='administrativo.php';</script>";
         }
 
 
-        $id = $_SESSION['id_admin'];
-
-        $sql = "UPDATE administrador SET img_profile=:img_profile WHERE id=$id";
-        $statement = $db->connect()->prepare($sql);
-        $statement->execute(array(':img_profile' => $_FILES['file']['name']));
-        //$_SESSION['imagen_profile']=$VARIABLE_HOST;
-
-        $_SESSION['img_profile'] = $statement;
-
-        $query = $db->connect()->prepare('SELECT * FROM administrador WHERE correo = :correo');
-        $query->execute(['correo' => $_SESSION['correo']]);
-
-        $row = $query->fetch(PDO::FETCH_NUM);
-
-        //session_destroy('imagen_profile');
-        //session_start();
-        $_SESSION['img_profile'] = $row[6];
-        echo "<script>location.href='administrativo.php';</script>";
     }
 }
 ob_end_flush();
